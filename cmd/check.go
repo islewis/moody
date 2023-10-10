6b962b6c01f8e55e68902c5d1e83cd7a6110e95d
coolacdc <isaiah@roof12.com>
2023-10-10 17:27:07 -0600

    Add WIP rewrite into methods

--- cmd/check.go
+++ cmd/check.go
@@ -14,8 +14,91 @@ import (
 	"time"
 )
 
+type Checks interface {
+    CollectInput() string
+    WriteToDB()
+}
+
+type BoolCheck struct{
+	Desc string
+}
+
+func (p *BoolCheck) CollectInput() {
+    fmt.Println("I am validating input!")
+}
+
+func (p *BoolCheck) WriteToDB() {
+    fmt.Println("I am writing to the DB!")
+}
+
+type IntCheck struct{
+	Desc string
+}
+
+func (p *IntCheck) CollectInput() {
+    fmt.Println("I am writing to the DB!")
+}
+
+func (p *IntCheck) WriteToDB() {
+    fmt.Println("I am writing to the DB!")
+}
+
+type RangeCheck struct{
+	Desc string
+	Max int 
+	Min int 
+}
+
+func (p *RangeCheck) CollectInput(name string) {
+	// Before we take an range input, confirm we have a maximum and a minimum defined in the config
+	maxValLoc := ("checks." + name + ".max")
+	if viper.GetString(maxValLoc) == "" {
+		errorMessage := "ERROR: the " + name + " definition is missing a maximum value.\n"
+		errorMessage += "All 'range' types need to have a max and a min defined. This should be " + maxValLoc + " in your config\n."
+		log.Fatalf(errorMessage)
+	}
+
+	minValLoc := ("checks." + name + ".min")
+	if viper.GetString(maxValLoc) == "" {
+		errorMessage := "ERROR: the " + name + " definition is missing a minimum value.\n"
+		errorMessage += "All 'range' types need to have a max and a min defined. This should be " + minValLoc + " in your config\n."
+		log.Fatalf(errorMessage)
+	}
+
+	// Get input until we have a value that is within our valid range
+	for {
+		fmt.Println(viper.GetString("checks." + name + ".description"))
+		fmt.Print(name + ": ")
+		var input int
+		fmt.Scanln(&input)
+		//input, err := strconv.Atoi(input)
+		//if err != nil {
+		//	log.Fatal(err)
+		//}
+
+		maxVal, _ := strconv.Atoi(viper.GetString(maxValLoc))
+		minVal, _ := strconv.Atoi(viper.GetString(minValLoc))
+		if input > maxVal {
+			fmt.Println("\n")
+			fmt.Println("Input is greater than the maximum of " + viper.GetString(maxValLoc) + ", try again.")
+			fmt.Println("\n")
+			continue
+		} else if input < minVal {
+			fmt.Println("Input is less than than the minimum of " + viper.GetString(minValLoc) + ", try again.")
+			continue
+		} else {
+			break
+		}
+	}
+    fmt.Println("I am writing to the DB!")
+}
+
+func (p *RangeCheck) WriteToDB() {
+    fmt.Println("I am writing to the DB!")
+}
+
 var checkinCmd = &cobra.Command{
-	Use:   "checkin",
+	Use:   "check",
 	Short: "Record mood, as well as and other daily checks",
 	Long:  "TO DO",
 	Run: func(cmd *cobra.Command, args []string) {
@@ -30,9 +113,10 @@ var checkinCmd = &cobra.Command{
 		var checks []string
 		for key := range checkMap {
 			checks = append(checks, key)
+			checks = append(checks, key)
 		}
 
-		// Iterate through checks
+		// Iterate through checks to create 
 		for _, check := range checks {
 			// Validate input differently for each data type
 			if viper.GetString("checks."+check+".type") == "range" {
